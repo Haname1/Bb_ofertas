8127a32a4ff4d2f48214a48aaef3652a73e66eee
Haname1 <151465459+Haname1@users.noreply.github.com>
2024-04-04 10:53:08 -0500

    Add files via upload

---
+++ bd_ofertas.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2024 a las 17:36:12
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ofertas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresas`
+--
+
+CREATE TABLE `empresas` (
+  `id` int(3) NOT NULL,
+  `razon_social` varchar(128) NOT NULL DEFAULT '',
+  `ruc` varchar(11) NOT NULL DEFAULT '',
+  `correo` varchar(64) NOT NULL DEFAULT '',
+  `dirección` varchar(128) NOT NULL DEFAULT '',
+  `telefono` varchar(10) NOT NULL DEFAULT '',
+  `descripción` varchar(500) NOT NULL DEFAULT '',
+  `rubro` varchar(64) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ofertas`
+--
+
+CREATE TABLE `ofertas` (
+  `id` int(5) NOT NULL,
+  `titulo` varchar(24) NOT NULL,
+  `id_empresa` int(3) DEFAULT NULL,
+  `pago` decimal(10,0) DEFAULT NULL,
+  `horario` varchar(6) NOT NULL,
+  `especialidad` varchar(100) NOT NULL DEFAULT '',
+  `modalidad` varchar(125) NOT NULL DEFAULT '',
+  `fecha_publicación` date DEFAULT NULL,
+  `fecha_cierre` date DEFAULT NULL,
+  `maximo_postulantes` int(57) NOT NULL,
+  `cantidad_postulantes` int(48) NOT NULL,
+  `estado` varchar(25) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulaciones`
+--
+
+CREATE TABLE `postulaciones` (
+  `id` int(3) NOT NULL,
+  `id_postulante` int(3) DEFAULT NULL,
+  `id_oferta` int(3) DEFAULT NULL,
+  `fecha_de_postulacion` datetime(6) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulantes`
+--
+
+CREATE TABLE `postulantes` (
+  `id` int(3) NOT NULL,
+  `DNI` int(9) DEFAULT NULL,
+  `nombres` varchar(60) NOT NULL DEFAULT '',
+  `apellidos` varchar(60) NOT NULL DEFAULT '',
+  `correo` varchar(128) NOT NULL DEFAULT '',
+  `teléfono` varchar(10) DEFAULT '',
+  `dirección` varchar(128) NOT NULL DEFAULT '',
+  `estado_civil` varchar(40) NOT NULL DEFAULT '',
+  `especialidad` varchar(45) NOT NULL DEFAULT '',
+  `archivo_cv` varchar(128) NOT NULL,
+  `edad` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `postulaciones`
+--
+ALTER TABLE `postulaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `postulantes`
+--
+ALTER TABLE `postulantes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postulaciones`
+--
+ALTER TABLE `postulaciones`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postulantes`
+--
+ALTER TABLE `postulantes`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
